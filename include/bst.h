dc69d6399b60b9ea6c897680272023696ec25553
LisaKirill <lisovets2008@gmail.com>
2026-05-29 23:39:03 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,4 +2,36 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
+#include <string>
+#include <vector>
+#include <utility>
+
+template <typename T>
+class BST {
+ private:
+  struct Node {
+    T value;
+    int count;
+    Node* left;
+    Node* right;
+    explicit Node(T val);
+  };
+
+  Node* root;
+
+  void destroyTree(Node* node);
+  Node* insertNode(Node* node, const T& val);
+  int calcDepth(Node* node) const;
+  bool searchNode(Node* node, const T& val) const;
+  void inorderTraverse(Node* node, std::vector<std::pair<int, T>>& vec) const;
+
+ public:
+  BST();
+  ~BST();
+  void insert(const T& val);
+  int depth() const;
+  bool search(const T& value) const;
+  std::vector<std::pair<int, T>> getFreqList() const;
+};
+
 #endif  // INCLUDE_BST_H_
